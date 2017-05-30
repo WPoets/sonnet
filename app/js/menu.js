6620e865e0b71586cba5b45a3f87d65164b1e129
Amit <amit@wpoets.com>
2017-05-30 13:05:40 +0530

    Initial Commit
    Initial commit with a basic working version.

---
+++ app/js/menu.js
@@ -0,0 +1,40 @@
+/*var remote = require('electron').remote;
+const {Menu, MenuItem} = remote;
+
+// Build our new menu
+var menu = new Menu()
+menu.append(new MenuItem({
+  label: 'Delete',
+  click: function() {
+    // Trigger an alert when menu item is clicked
+    alert('Deleted')
+  }
+}))
+menu.append(new MenuItem({
+  label: 'More Info...',
+  click: function() {
+    // Trigger an alert when menu item is clicked
+    alert('Here is more information')
+  }
+}))
+
+ // Add the listener
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('.js-context-menu').addEventListener('click', function (event) {
+    menu.popup(remote.getCurrentWindow());
+  })
+})
+ */
+
+const {remote} = require('electron')
+const {Menu, MenuItem} = remote
+
+const menu = new Menu()
+menu.append(new MenuItem({label: 'MenuItem1', click() { console.log('item 1 clicked') }}))
+menu.append(new MenuItem({type: 'separator'}))
+menu.append(new MenuItem({label: 'MenuItem2', type: 'checkbox', checked: true}))
+
+window.addEventListener('contextmenu', (e) => {
+  e.preventDefault()
+  menu.popup(remote.getCurrentWindow())
+}, false)
