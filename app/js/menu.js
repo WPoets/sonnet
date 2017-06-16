c5230c708c1c2582e35ce32746461a5bdab31f7c
anirudha-prabhune <anirudha.prabhune@gmail.com>
2017-06-16 18:48:33 +0530

    Added Reload button in menu

--- app/js/menu.js
+++ app/js/menu.js
@@ -30,9 +30,9 @@ const {remote} = require('electron')
 const {Menu, MenuItem} = remote
 
 const menu = new Menu()
-menu.append(new MenuItem({label: 'MenuItem1', click() { console.log('item 1 clicked') }}))
+menu.append(new MenuItem({label: 'Back', click: () => windows.main.dispatch('escapeBack')}))
 menu.append(new MenuItem({type: 'separator'}))
-menu.append(new MenuItem({label: 'MenuItem2', type: 'checkbox', checked: true}))
+menu.append(new MenuItem({label: 'Reload', role: "reload"}))
 
 window.addEventListener('contextmenu', (e) => {
   e.preventDefault()
